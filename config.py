a0b144d0d6799152300b606385837b291923fc58
VladimirOnCreate <vladimir.muravjov@gmail.com>
2017-07-06 22:50:31 +0300

    add check host name

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
+import socket
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+if socket.gethostname() == "MacBook-Air-Vladimir.local":
+    PRODUCTION = False
+else:
+    PRODUCTION = True
+
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://oncreate:0987654321qwerty@oncreate.mysql.pythonanywhere-services.com/oncreate$oncreate_db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+if PRODUCTION:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://oncreate:0987654321qwerty@oncreate.mysql.pythonanywhere-services.com/oncreate$oncreate_db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
